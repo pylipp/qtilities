19cb68e33f244e57d32fe25b84fb4949a736870f
pylipp <beth.aleph@yahoo.de>
2018-03-25 11:03:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from setuptools import setup, find_packages
+from qtilities import __version__
+
+with open("README.md") as readme:
+    long_description = readme.read()
 
 setup(
-        name='qtilities',
-        version='0.1',
-        description='TODO',
-        url='http://github.com/pylipp/qtilities',
-        author='Philipp Metzner',
-        author_email='beth.aleph@yahoo.de',
-        license='GPLv3',
-        #classifiers=[],
-        packages=find_packages(exclude=['test', 'doc']),
-        entry_points = {
-            'console_scripts': ['qtilities = qtilities.main:main']
-            },
-        install_requires=[]
-        )
+    name='qtilities',
+    version=__version__,
+    description='TODO',
+    long_description=long_description,
+    url='http://github.com/pylipp/qtilities',
+    author='Philipp Metzner',
+    author_email='beth.aleph@yahoo.de',
+    license='GPLv3',
+    # classifiers=[],
+    packages=find_packages(exclude=['test', 'doc']),
+    scripts=["bin/pqp", "bin/pqpc"],
+    entry_points={
+        # 'console_scripts': ['qtilities = qtilities.main:main']
+        },
+    install_requires=[
+        "PyQt5",
+    ]
+)
