b8727d3bd28ef479e5790f09c24d25f868a6c0a2
pylipp <philipp.metzner@tum.de>
2020-12-23 14:04:55 +0100

    Change verifying expected qmltags content
    Global list is easier to updated and compare

--- test/test_qmltags.py
+++ test/test_qmltags.py
@@ -5,6 +5,14 @@ import tempfile
 from qtilities.qmltags import main as qmltags
 
 
+EXPECTED_CONTENT = """\
+TheWindow\ttest/TheWindow.qml\t/^ApplicationWindow {$/;"\tc
+doNothing\ttest/TheWindow.qml\t/^    function doNothing() {}$/;"\tm\tclass:TheWindow
+doLess\ttest/TheWindow.qml\t/^    function doLess(argument) {}$/;"\tm\tclass:TheWindow
+nested\ttest/TheWindow.qml\t/^        function nested() {}$/;"\tm\tclass:TheWindow
+""".splitlines()
+
+
 class QmltagsTestCase(unittest.TestCase):
 
     def test_generating_qmltags_file(self):
@@ -16,14 +24,9 @@ class QmltagsTestCase(unittest.TestCase):
         )
 
         with open(output_filepath) as f:
-            actual_content = f.read()
-
-        excepted_content = \
-            'TheWindow\ttest/TheWindow.qml\t/^ApplicationWindow {$/;"\tc\n' \
-            'doNothing\ttest/TheWindow.qml\t/^    function doNothing() {}$/;"\tm\tclass:TheWindow\n' \
-            'doLess\ttest/TheWindow.qml\t/^    function doLess(argument) {}$/;"\tm\tclass:TheWindow\n' \
-            'nested\ttest/TheWindow.qml\t/^        function nested() {}$/;"\tm\tclass:TheWindow'
-        self.assertEqual(actual_content, excepted_content)
+            actual_content = f.readlines()
+            actual_content = [l.rstrip('\n') for l in actual_content]
+        self.assertEqual(actual_content, EXPECTED_CONTENT)
 
         os.unlink(output_filepath)
 
